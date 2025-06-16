79d936614eb56488abfeba0c9c4b69593864f7e8
milina <milinashakya@gmail.com>
2025-06-16 11:05:01 -0400

    Add files via upload

---
+++ Sales and Product/Sales and Product/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sales_and_Product.Models;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace Sales_and_Product.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Sales and Product.csv");
+            try
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    PrepareHeaderForMatch = args => args.Header.Replace(" ", "")
+                };
+
+                using var reader = new StreamReader(filePath);
+                using var csv = new CsvHelper.CsvReader(reader, config);
+
+                var newSalesItem = csv.GetRecords<SalesItems>().ToList();
+
+                return Ok(newSalesItem);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
+
---
+++ Sales and Product/Sales and Product/Models/SalesItems.cs
@@ -0,0 +1,16 @@
+﻿namespace Sales_and_Product.Models
+{
+    public class SalesItems
+    {        
+        public string? ProductName { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Color { get; set; }
+        public string? Brand { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string? FiscalMonthOfYearName { get; set; }
+        public int? SalesQuantity { get; set; }
+        public double? SalesAmount { get; set; }
+        public int? CustomerCount { get; set; }
+        public int? DeliveredQuantity { get; set; }
+    }
+}
